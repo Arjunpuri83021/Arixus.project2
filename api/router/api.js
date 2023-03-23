124ee2e81288ae7c4f38cce38a93cc4675e5a9c7
arjun puri <thevirus991@gmail.com>
2023-03-23 13:31:44 +0530

    Initial commit

--- api/router/api.js
+++
@@ -1,7 +0,0 @@
-const router=require('express').Router()
-const { Router } = require('express')
-const cUser=require('../controlers/user.controler')
-router.post('/reg',cUser.userinsert)
-router.post('/login',cUser.logininsert)
-
-module.exports = router
